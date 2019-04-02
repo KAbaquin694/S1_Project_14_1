d88eeb51100d7a2e0e660fe3c40a61d8dd10a2f8
Khalel Abaquin <kabaquin694@west-mec.org>
2019-04-02 15:28:51 -0700

    First Update
    renamed 2 files, added name & date, added script element to bottom of html, added event listeners, added a function, added a for loop, added an anonymous function, created element nodes, created  style nodes, set attributes of nodes, added final comments

---
+++ na_styler.js
@@ -0,0 +1,78 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3 and JavaScript 6th Edition
+   Tutorial 12
+   Case Problem 1
+
+   Author: Khalel Abaquin
+   Date:   4.2.19
+
+   Filename: na_styler.js
+
+   Functions
+   =========
+   
+   setStyles()
+      Sets up the style sheets and the style sheet switcher.
+      
+   randInt(size)
+      Returns a random integer from 0 up to size-1.
+
+*/
+
+//This line of code loads the funtion 'setStyles' every time the page loads.
+window.addEventListener("load", setStyles);
+
+//This section of code creates the function 'setStyles', which deals with setting the style rules & sheets.
+function setStyles() {
+      var styleNum = randInt(5);
+      var sheetLink = document.createElement("link");
+      sheetLink.setAttribute("rel", "stylesheet");
+      sheetLink.setAttribute("id", "fancySheet");
+      sheetLink.setAttribute("href", "na_style_" + styleNum + ".css");
+      document.head.appendChild(sheetLink);
+      var figBox = document.createElement("figure");
+      figBox.setAttribute("id", "styleThumbs");
+      document.getElementById("box").appendChild(figBox);
+
+      //This block of code sets a different image for the webpage out of 5 different images, depending on which ine the user chooses. 
+      for (var i = 0; i <= 5; i++) {
+            var sheetImg = document.createElement("img");
+            sheetImg.setAttribute("src", "na_small_" + [i] + ".png");
+            sheetImg.setAttribute("alt", "na_style_" + [i] + ".css");
+            sheetImg.addEventListener("click", function (e) {
+                  sheetLink.setAttribute("href", "na_style_" + e.target.alt + ".css");
+            });
+            figBox.appendChild(sheetImg);
+      }
+
+      //This piece of code creates an embedded style sheet, appends it to the head of the document, & stores it inside the variable 'thumbStyles'.
+      var thumbStyles = document.createElement("style");
+      document.head.appendChild(thumbStyles);
+
+      //These block of code create the css style rules for the web page.
+      document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "figure#styleThumbs { \
+                  position: absolute; \
+                  left: 0px; \
+                  bottom: 0px; \
+            }", 0);
+
+      document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "figure#styleThumbs img { \
+                  outline: 1px solid black; \
+                  cursor: pointer; \
+                  opacity: 0.75; \
+            }", 1);
+
+      document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "figure#styleThumbs img:hover { \
+                  outline: 1px solid red; \
+                  opacity: 1.0; \
+            }", 2);
+}
+
+function randInt(size) {
+      return Math.floor(size * Math.random());
+}
